506b86ed9a97041bfe35c2480c2fb7befc9a789e
Noah Maktabi <mctabi@me.com>
2021-10-05 14:32:35 +0200

    Cleaned Program.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -24,35 +24,6 @@ namespace Presentation
             
             // Search test
             //var results = manager.FindParkedVehicles("volv");
-            //foreach (var result in results)
-            //{
-            //    Console.WriteLine(result.Make);
-            //    Console.WriteLine(result.Model);
-            //    Console.WriteLine(result.Year);
-            //}
-            
-            //Console.WriteLine($"Available spots: {garage.ParkingSpots.Count(s => s.IsAvailable)}");
-
-            //IVehicleDetailsGetter getter = new VehicleDetailsGetter();
-            //var car = getter.GetCarDetails();
-            IVehicleDisplay display = new VehicleDisplay();
-            //display.ShowVehicleDetails(car);
-
-            // var response = await manager.ParkAsync(bus);
-
-            //Console.WriteLine("\n\n\nNew Entry");
-            //Console.WriteLine($"Available spots: {garage.ParkingSpots.Count(s => s.IsAvailable)}");
-            //foreach (var vehicle in garage)
-            //{
-            //    Console.WriteLine($"RegNr: {vehicle.LicensePlate} -- ParkingSpot: {vehicle.ParkingSpotNumber}");
-            //}
-
-            //await DataHandler.AddToParkedVehiclesAsync(nCar, VehicleType.Car);
-            //list = await DataHandler.GetParkedVehiclesAsync();
-            //list.ForEach(c =>
-            //{
-            //    Console.WriteLine(c.LicensePlate);
-            //});
         }
     }
 }
